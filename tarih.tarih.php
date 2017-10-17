f8e6a1591641da62d4a397bb42ebca3887da8d96
Penerovic-huseyn-kuzu <32458792+Penerovic-huseyn-kuzu@users.noreply.github.com>
2017-10-17 19:53:02 +0300

    tarih_tarih.Php

---
+++ tarih.tarih.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+$t1 =strtotime("Nov 11");
+$t2 =strtotime("December 25")
+$t3 = strtotime("11 may 2018")
+ $kacGun = ceil(($t1-time())/60/60/24);
+ echo "Vizeye $kacGun gün var <br>";
+$kacGun = ceil(($t1-time())/60/60/24);
+echo "finale $kacGun gün var <br>";
+$kacGun = ceil(($t1-time())/60/60/24);
+echo "Mezun olmama $kacGun gün var <br>";
+
+?>
+<?php
+$baslama = strtotime("17 10 2017");
+$bitis = strtotime("+6 weeks", $baslama);
+
+while ($baslama < $enddate) {
+  echo date("M d", $baslama) . "<br>";
+  $baslama = strtotime("+1 week", $baslama);
+}
+?>
+</body>
+</html>
